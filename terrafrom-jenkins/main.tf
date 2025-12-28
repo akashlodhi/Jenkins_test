fef5fc60a9d6e44df43807621838b1be1ee6fb18
akash <akash91lodhi@live.com>
2025-12-28 13:51:47 +0530

    Terraform building the pipeline

---
+++ terrafrom-jenkins/main.tf
@@ -0,0 +1,102 @@
+########################################
+# Security Group for Jenkins
+########################################
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and Jenkins UI"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+########################################
+# IAM Role for Jenkins EC2
+########################################
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+########################################
+# IAM Policy (Admin - Lab Purpose)
+########################################
+resource "aws_iam_policy" "jenkins_policy" {
+  name = "jenkins-terraform-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "*"
+      Resource = "*"
+    }]
+  })
+}
+
+########################################
+# Attach Policy to Role
+########################################
+resource "aws_iam_role_policy_attachment" "jenkins_policy_attach" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.jenkins_policy.arn
+}
+
+########################################
+# Instance Profile (Required for EC2)
+########################################
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkins-instance-profile"
+  role = aws_iam_role.jenkins_role.name
+}
+
+########################################
+# Jenkins EC2 Instance
+########################################
+resource "aws_instance" "jenkins_pipeline" {
+  ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2 (ap-south-1)
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  iam_instance_profile  = aws_iam_instance_profile.jenkins_profile.name
+
+  user_data = file("jenkins-install.sh")
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
---
+++ terrafrom-jenkins/variables.tf
@@ -0,0 +1,14 @@
+variables "aws_region" {
+    description = "AWS region"
+    default = "ap-south-1"
+}
+
+variable "instance_type" {
+    description = "ec2 Instance type"
+    default = "t2.medium"
+}
+
+variable "key_Pair"{
+    description = "key_Pair"
+    default = "stark_jenkins"
+}
